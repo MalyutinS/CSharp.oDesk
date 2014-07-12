8567d75becf626bcbb0daa722c84d00f2139455c
Sergey <MalyutinS@gmail.com>
2014-07-12 14:11:46 +0200

    Store Odesk api keys and obtained access token in settings

--- CSharp.oDesk.Console Example/Program.cs
+++ CSharp.oDesk.Console Example/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using CSharp.oDesk.Api;
 using CSharp.oDesk.Connect;
+using CSharp.oDesk.Console_Example.Properties;
 using Spring.Json;
 using Spring.Social.OAuth1;
 
@@ -9,37 +10,51 @@ namespace CSharp.oDesk.Console_Example
 {
     class Program
     {
-        // Set your consumer key & secret here
-        private const string ODeskApiKey = "ENTER YOUR KEY HERE";
-        private const string ODeskApiSecret = "ENTER YOUR SECRET HERE";
+        // Please, don't forget to set your consumer key & secret here in setting file
 
         private static void Main()
         {
             try
             {
-                var oDeskServiceProvider = new oDeskServiceProvider(ODeskApiKey, ODeskApiSecret);
 
-                /* OAuth 'dance' */
+                var oDeskServiceProvider = new oDeskServiceProvider(Settings.Default.ODeskApiKey, Settings.Default.ODeskApiSecret);
 
-                // Authentication using Out-of-band/PIN Code Authentication
-                Console.Write("Getting request token...");
-                var oauthToken = oDeskServiceProvider.OAuthOperations.FetchRequestTokenAsync("oob", null).Result;
-                Console.WriteLine("Done");
+                if (string.IsNullOrEmpty(Settings.Default.ODeskAccessTokenValue) ||
+                    string.IsNullOrEmpty(Settings.Default.ODeskAccessTokenSecret))
+                {
+                    /* OAuth 'dance' */
 
-                var authenticateUrl = oDeskServiceProvider.OAuthOperations.BuildAuthorizeUrl(oauthToken.Value, null);
-                Console.WriteLine("Redirect user for authentication: " + authenticateUrl);
-                Process.Start(authenticateUrl);
-                Console.WriteLine("Enter PIN Code from oDesk authorization page:");
-                var pinCode = Console.ReadLine();
+                    // Authentication using Out-of-band/PIN Code Authentication
+                    Console.Write("Getting request token...");
+                    var oauthToken = oDeskServiceProvider.OAuthOperations.FetchRequestTokenAsync("oob", null).Result;
+                    Console.WriteLine("Done");
 
-                Console.Write("Getting access token...");
-                var requestToken = new AuthorizedRequestToken(oauthToken, pinCode);
-                var oauthAccessToken = oDeskServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
-                Console.WriteLine("Done");
+                    var authenticateUrl = oDeskServiceProvider.OAuthOperations.BuildAuthorizeUrl(oauthToken.Value, null);
+                    Console.WriteLine("Redirect user for authentication: " + authenticateUrl);
+                    Process.Start(authenticateUrl);
+                    Console.WriteLine("Enter PIN Code from oDesk authorization page:");
+                    var pinCode = Console.ReadLine();
+
+                    Console.Write("Getting access token...");
+                    var requestToken = new AuthorizedRequestToken(oauthToken, pinCode);
+                    var oauthAccessToken = oDeskServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
+                    Console.WriteLine("Done");
+
+                    Settings.Default.ODeskAccessTokenValue = oauthAccessToken.Value;
+                    Settings.Default.ODeskAccessTokenSecret = oauthAccessToken.Secret;
+                    Settings.Default.Save();
+                }
+                else
+                {
+                    Console.Write("Getting access token from settings");
+                    Console.WriteLine("Done");
+                }
+
+                
 
                 /* API */
 
-                var oDesk = oDeskServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+                var oDesk = oDeskServiceProvider.GetApi(Settings.Default.ODeskAccessTokenValue, Settings.Default.ODeskAccessTokenSecret);
 
                 oDesk.RestOperations.GetForObjectAsync<JsonValue>("https://www.odesk.com/api/auth/v1/info.json")
                     .ContinueWith(task => Console.WriteLine("Result: " + task.Result.ToString()));
